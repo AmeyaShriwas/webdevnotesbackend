1959da0d95923cab4b3ff5b3fb5bcf2275e5a0af
AmeyaShriwas133 <shriwas.ameya@webseeder.in>
2024-10-13 17:18:23 +0530

    n

--- Models/ContactUsModel.js
+++ Models/ContactUsModel.js
@@ -3,12 +3,15 @@ const mongoose = require('mongoose')
 const ContactUsSchema = new mongoose.Schema({
     name: {
         type: String,
+        required: true
     },
     id: {
         type: String,
+        required: true
     },
     message: {
         type: String,
+        required: true
     }
 })
 
--- index.js
+++ index.js
@@ -20,6 +20,27 @@ app.get('/', async(req, resp)=> {
     resp.send('api is working')
 })
 
+app.delete('/delete-collection/:collectionName', async (req, res) => {
+  const { collectionName } = req.params;
+
+  try {
+      const db = mongoose.connection.db;
+      const result = await db.dropCollection(collectionName);
+
+      if (result) {
+          return res.status(200).json({ message: `${collectionName} collection deleted successfully.` });
+      } else {
+          return res.status(404).json({ message: `Collection ${collectionName} not found.` });
+      }
+  } catch (error) {
+      if (error.code === 26) {
+          return res.status(404).json({ message: `Collection ${collectionName} does not exist.` });
+      } else {
+          return res.status(500).json({ message: `Error deleting collection: ${error.message}` });
+      }
+  }
+});
+
 app.use('/', authRoutes)
 
 app.listen(process.env.PORT, ()=> {
