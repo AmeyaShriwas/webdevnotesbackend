2192c5e596e22c3e742ff123d2f2320b86dfeaa6
AmeyaShriwas133 <shriwas.ameya@webseeder.in>
2024-10-13 17:09:15 +0530

    new

--- Models/ContactUsModel.js
+++ Models/ContactUsModel.js
@@ -3,15 +3,12 @@ const mongoose = require('mongoose')
 const ContactUsSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
     },
     id: {
         type: String,
-        required: true
     },
     message: {
         type: String,
-        required: true
     }
 })
 
