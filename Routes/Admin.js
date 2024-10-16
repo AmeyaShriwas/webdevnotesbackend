f24c19d0cf610afd6177a242ee78e68cbbfca9ec
AmeyaShriwas133 <shriwas.ameya@webseeder.in>
2024-10-16 13:43:23 +0530

    new

--- Routes/Admin.js
+++ Routes/Admin.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/pdf'); // Directory to store PDFs
+    cb(null, 'uploads'); // Directory to store PDFs
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
