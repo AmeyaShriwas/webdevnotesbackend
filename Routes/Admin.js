8aa3c4dc02e1ef8b8950fba6136d2a5e5ae3c9b1
AmeyaShriwas133 <shriwas.ameya@webseeder.in>
2024-10-16 15:46:58 +0530

    new

--- Routes/Admin.js
+++ Routes/Admin.js
@@ -14,7 +14,7 @@ if (!fs.existsSync(uploadDir)) {
 // Multer config for storing PDFs
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads'); // Ensure correct file path
+    cb(null, './uploads'); // Ensure correct file path
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
