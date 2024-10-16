a5d35745e93aa1cf731503147d9880aa44a72531
AmeyaShriwas133 <shriwas.ameya@webseeder.in>
2024-10-16 14:56:44 +0530

    new

--- Routes/Admin.js
+++ Routes/Admin.js
@@ -5,31 +5,33 @@ const Auth = require('./../Middleware/Auth');
 const multer = require('multer');
 const path = require('path');
 
+// Multer config for storing PDFs
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads'); // Directory to store PDFs
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname)); // Store file with a unique name
-  }
-});
-
-// File filter to allow only PDFs
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype === 'application/pdf') {
-    cb(null, true);
-  } else {
-    cb(new Error('Only PDF files are allowed!'), false);
-  }
-};
-
-// Limit file size to 20MB
-const upload = multer({
-  storage: storage,
-  limits: { fileSize: 1024 * 1024 * 20 }, // 20MB limit
-  fileFilter: fileFilter,
-});
+    destination: function (req, file, cb) {
+      cb(null, '/uploads'); // Directory to store PDFs
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+      cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname)); // Store the file with a unique name
+    }
+  });
+  
+  // File filter to allow only PDFs
+  const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'application/pdf') {
+      cb(null, true);
+    } else {
+      cb(new Error('Only PDF files are allowed!'), false);
+    }
+  };
+  
+  // Limit file size to 5MB
+  const upload = multer({
+    storage: storage,
+    limits: { fileSize: 1024 * 1024 * 20 }, // 20MB limit
+    fileFilter: fileFilter,
+  });
+  
 
 
 router.post('/admin/grantAdminAccess', Auth, AdminController.AdminAccessController)
